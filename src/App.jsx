b31be21706594d069e16a9587679a9cccf9c0e6f
MartinSaporiti99 <martinsaporiti45@gmail.com>
2025-04-26 00:16:01 -0300

    add routers

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,13 @@ import { useState } from "react";
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Footer from "./components/footer/Footer";
+import Home from "./components/home/Home";
+import Login from "./components/user/login/Login";
+import Register from "./components/user/register/Register";
+import Cart from "./components/cart/Cart";
+
 import GigantCarrousel from "./components/gigantCarrousel/gigantCarrousel";
+import { BrowserRouter, Route, Routes } from "react-router";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -10,9 +16,14 @@ function App() {
     <>
       <Navbar></Navbar>
       <GigantCarrousel></GigantCarrousel>
-      <div>
-        <p>ESPACIO</p>
-      </div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/login" element={<Login />}></Route>
+          <Route path="/register" element={<Register />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+        </Routes>
+      </BrowserRouter>
       <Footer></Footer>
     </>
   );
