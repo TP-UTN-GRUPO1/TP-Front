3d8610ff2c9a573058f54e7433f584cfba3dce47
MartinSaporiti99 <martinsaporiti45@gmail.com>
2025-04-27 14:28:16 -0300

    styles nav & footer

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { House, Cart, Heart } from "react-bootstrap-icons";
-import "./Navbar.css";
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <nav className="navBar">
-      <div className="navBarTop">
+    <nav className="navbar">
+      <div className="navbar-top">
         <div className="navbar-left">
-          <House size={28} className="icon" />
-          <span className="logo">TheFrog</span>
+          <Link to="/" className="link-button">
+            <button>
+              <House size={28} className="icon" />
+              <span className="logo">TheFrog</span>
+            </button>
+          </Link>
         </div>
 
         <input
@@ -21,15 +25,16 @@ const Navbar = () => {
         <div className="navbar-right">
           <Cart size={24} className="icon" />
           <Heart size={24} className="icon" />
-          <Link to="./login">
+          <Link to="/login">
             <button className="nav-button">Mi Cuenta</button>
           </Link>
-          <Link to="./register">
+          <Link to="/register">
             <button className="nav-button primary">Registrarse</button>
           </Link>
         </div>
       </div>
-      <div className="navBarBot">
+
+      <div className="navbar-bot">
         <button className="button-console">PS5</button>
         <button className="button-console">PS4</button>
         <button className="button-console">Nintendo</button>
