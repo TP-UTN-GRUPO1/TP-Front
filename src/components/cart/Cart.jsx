5f32ec571ebef1fafd98dc8c1ddf4990c3c64ba5
unknown <qalopez2000@gmail.com>
2025-05-21 15:58:05 -0300

    Cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,6 +1,11 @@
 import "./Cart.css";
 
 const Cart = () => {
+
+  /* generar un estado donde vaya guardando la info del producto precio, nombre del juego y
+   plataforma seleccionada, para luego generar una OC o un pago a mercado libre (la OC es la opcion mas rapida la de meli es
+   por si tenemos tiempo), dentro del carrito se pueda agregar + de un producto y eliminar juegos seleccionados, ademas de que vaya sumando
+   el total de los juegos  */
   return <div>Cart</div>;
 };
 
