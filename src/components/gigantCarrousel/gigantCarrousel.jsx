2bbda2df4e9d7b28c3eb24f65443c554b490ddc4
MartinSaporiti99 <martinsaporiti45@gmail.com>
2025-05-22 11:42:54 -0300

    Fixed carousel movement

--- src/components/gigantCarrousel/gigantCarrousel.jsx
+++ src/components/gigantCarrousel/gigantCarrousel.jsx
@@ -6,17 +6,6 @@ const gigantCarrousel = () => {
   const listRef = useRef();
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  useEffect(() => {
-    const listNode = listRef.current;
-    const imgNode = listNode.querySelectorAll("li > img")[currentIndex];
-
-    if (imgNode) {
-      imgNode.scrollIntoView({
-        behavior: "smooth",
-      });
-    }
-  }, [currentIndex]);
-
   const scrollToImage = (direction) => {
     if (direction === "prev") {
       setCurrentIndex((curr) => {
@@ -44,14 +33,16 @@ const gigantCarrousel = () => {
           &#10093;
         </div>
         <div className="container-images">
-          <ul className="ul-gigantCarrousel" ref={listRef}>
-            {data.map((item) => {
-              return (
-                <li className="li-gigantCarrousel" key={item.id}>
-                  <img src={item.imageUrl} width={500} height={280} />
-                </li>
-              );
-            })}
+          <ul
+            className="ul-gigantCarrousel"
+            ref={listRef}
+            style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+          >
+            {data.map((item) => (
+              <li className="li-gigantCarrousel" key={item.id}>
+                <img src={item.imageUrl} width={500} height={280} />
+              </li>
+            ))}
           </ul>
         </div>
         <div className="dots-container">
