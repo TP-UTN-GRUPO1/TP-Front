c46480b80c0d386317cdf0ada90169033a3705e5
unknown <qalopez2000@gmail.com>
2025-05-24 00:52:35 -0300

    Dashboard in progress

---
+++ src/components/dashboard/Dashboard.jsx
@@ -0,0 +1,26 @@
+/*El objetivo de este componente es crear un panel donde el usuario/admin/sysadmin puedan modificar datos segun el role
+asigando es decir que el usuario solo pueda ver mis datos e historial de compras (este ultimo podriamos hacerlo solo si llegamos
+con lo importante), el admin ...usuario , historial,productos y el sysadmin todas las funciones mas que nada la de modificar usuarios */
+
+const Dashboard = () => {
+  return (
+    <div>
+        <h1>Panel administrativo</h1>
+        <>
+        <button>Mis Datos</button>
+        </>  
+        <>
+        <button>Historial de compras</button>
+        </>
+        <>
+        <button>Modificar Usuarios</button>
+        </>   
+        <>
+        <button>Modificar productos</button> 
+        
+        </>
+    </div>
+  )
+}
+
+export default Dashboard
