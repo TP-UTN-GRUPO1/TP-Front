4eb3b7180d312f36eb123f20a7a0f066ce1c93c5
unknown <qalopez2000@gmail.com>
2025-06-04 18:48:38 -0300

    dashboard in comming

--- src/App.jsx
+++ src/App.jsx
@@ -8,34 +8,43 @@ import Err from "./components/err/Err";
 import Cart from "./components/cart/Cart";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AuthContextProvider } from "./auth/AuthContextProvider";
-import { CartProvider } from "./components/cartContext/CartContext";
+
 import LayoutNavbar from "./components/nav/LayoutNavbar";
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+import { CartProvider } from "./components/CartContext/CartContext";
 
 function App() {
   return (
-    <>
-      <AuthContextProvider>
-        <CartProvider>
-          <BrowserRouter>
-            <Routes>
-              <Route element={<LayoutNavbar hideUserButtons={true} />}>
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/games/:id" element={<CardPage />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="*" element={<Err />} />
-              </Route>
-              <Route element={<LayoutNavbar />}>
-                <Route path="/" element={<Home />} />
-              </Route>
+    <AuthContextProvider>
+      <CartProvider>
 
+        <BrowserRouter>
+          <Routes>
+            <Route element={<LayoutNavbar hideUserButtons={true} />}>
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/games/:id" element={<CardPage />} />
+            </Route>
+            <Route element={<LayoutNavbar />}>
+              <Route path="/" element={<Home />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
-            </Routes>
-          </BrowserRouter>
-        </CartProvider>
+            </Route>
+            <Route path="*" element={<Err />} />
+              <Route path="/dashboard" element={<Dashboard />}>
+                <Route path="user" element={<Dashboard />} />
+                <Route path="products" element={<Dashboard />} />
+                <Route path="purchasedHistory" element={<Dashboard />} />
+                <Route path="account" element={<Dashboard/>}/>
+              </Route>
+          </Routes>
+        </BrowserRouter>
+        <ToastContainer />
+      </CartProvider>
+   
       </AuthContextProvider>
-    </>
   );
 }
 
 export default App;
+
--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,30 +1,34 @@
-/*El objetivo de este componente es crear un panel donde el usuario/admin/sysadmin puedan modificar datos segun el role
-asigando es decir que el usuario solo pueda ver mis datos e historial de compras (este ultimo podriamos hacerlo solo si llegamos
-con lo importante), el admin ...usuario , historial,productos y el sysadmin todas las funciones mas que nada la de modificar usuarios */
-
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Dashboard = () => {
   return (
     <div>
-        <h1>Mi Cuenta</h1>
-        <>
-        <button>Mis Datos</button>
-        </>  
-        <>
-        <button>Historial de compras</button>
-        </>
-        <>
-        <button>Modificar Usuarios</button>
-        </>   
-        <>
-        <button>Modificar productos</button>         
-        </>
-        <>
+      <h1>Mi Cuenta</h1>
+      <div>
+        <button>
+          <Link to="account">Mis Datos</Link>
+        </button>
+      </div>
+      <div>
+        <button>
+          <Link to="purchasedHistory">Historial de compras</Link>
+        </button>
+      </div>
+      <div>
+        <button>
+          <Link to="user">Modificar Usuarios</Link>
+        </button>
+      </div>
+      <div>
+        <button>
+          <Link to="products">Modificar productos</Link>
+        </button>
+      </div>
+      <div>
         <Link to="/">Volver</Link>
-        </>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
