cdc68636ad0b0a3cc4eb054d0c777d1d7a4837bb
santichieu <santi.chieu@gmail.com>
2025-06-15 17:42:59 -0300

    order by persistence fixed

--- src/components/platformFilters/PlatformFilters.jsx
+++ src/components/platformFilters/PlatformFilters.jsx
@@ -1,7 +1,11 @@
 import { NavDropdown } from "react-bootstrap";
 import { SORT_ORDERS } from "../home/Home.consts";
+import { ORDER_LABELS } from "./PlatformOrderLabel";
+import { useState } from "react";
 
 const PlatformFilters = ({ onFilter, selectedOrder, onselectedOrder }) => {
+  const [orderLabel, setOrderLabel] = useState("Ordenar por");
+
   const platforms = [
     "PS5",
     "PS4",
@@ -23,31 +27,51 @@ const PlatformFilters = ({ onFilter, selectedOrder, onselectedOrder }) => {
           {platform === "Xbox Series"
             ? "XBOX Series S|X"
             : platform === "Xbox One"
-            ? "XBOX ONE"
-            : platform}
+              ? "XBOX ONE"
+              : platform}
         </button>
       ))}
-
-      <NavDropdown className="button-console" menuVariant="dark">
+      {/* cambiar title por una funcion */}
+      <NavDropdown className="button-console" menuVariant="dark" title={orderLabel}>
         <NavDropdown.Item
           active={selectedOrder === SORT_ORDERS.LOW_TO_HIGH}
-          onClick={() => onselectedOrder(SORT_ORDERS.LOW_TO_HIGH)}
+          onClick={() => {
+            onselectedOrder(SORT_ORDERS.LOW_TO_HIGH);
+            setOrderLabel(ORDER_LABELS[SORT_ORDERS.LOW_TO_HIGH]);
+          }}
         >
           Menor-Mayor
         </NavDropdown.Item>
         <NavDropdown.Item
-          onClick={() => onselectedOrder(SORT_ORDERS.HIGH_TO_LOW)}
+          active={selectedOrder === SORT_ORDERS.HIGH_TO_LOW}
+          onClick={() => {
+            onselectedOrder(SORT_ORDERS.HIGH_TO_LOW);
+            setOrderLabel(ORDER_LABELS[SORT_ORDERS.HIGH_TO_LOW]);
+          }}
         >
           Mayor-Menor
         </NavDropdown.Item>
-        <NavDropdown.Item onClick={() => onselectedOrder(SORT_ORDERS.A_Z)}>
+        <NavDropdown.Item 
+        active={selectedOrder === SORT_ORDERS.A_Z}
+        onClick={() => {
+          onselectedOrder(SORT_ORDERS.A_Z);
+          setOrderLabel(ORDER_LABELS[SORT_ORDERS.A_Z])
+          }}>
           A-Z
         </NavDropdown.Item>
-        <NavDropdown.Item onClick={() => onselectedOrder(SORT_ORDERS.Z_A)}>
+        <NavDropdown.Item 
+        active={selectedOrder === SORT_ORDERS.Z_A}
+        onClick={() => {
+          onselectedOrder(SORT_ORDERS.Z_A);
+          setOrderLabel(ORDER_LABELS[SORT_ORDERS.Z_A])
+          }}>
           Z-A
         </NavDropdown.Item>
         <NavDropdown.Divider />
-        <NavDropdown.Item onClick={() => onselectedOrder(SORT_ORDERS.RESET)}>
+        <NavDropdown.Item onClick={() =>{
+          onselectedOrder(SORT_ORDERS.RESET);
+          setOrderLabel(ORDER_LABELS[SORT_ORDERS.RESET]);
+          }}>
           Reiniciar Filtros
         </NavDropdown.Item>
       </NavDropdown>
---
+++ src/components/platformFilters/PlatformOrderLabel.js
@@ -0,0 +1,13 @@
+import { SORT_ORDERS } from "../home/Home.consts";
+
+
+export const ORDER_LABELS = {
+  [SORT_ORDERS.LOW_TO_HIGH]: "Menor-Mayor",
+  [SORT_ORDERS.HIGH_TO_LOW]: "Mayor-Menor",
+  [SORT_ORDERS.A_Z]: "A-Z",
+  [SORT_ORDERS.Z_A]: "Z-A",
+  [SORT_ORDERS.RESET]: "Ordenar por"
+};
+
+
+
